b2d3925daf085460ef9a9ce3f2bf7e184832b603
Jonny Hall <jonny.hall@touchsurgery.com>
2018-09-13 11:46:12 +0100

    changed to vote system

--- firebase-demo/src/App.js
+++ firebase-demo/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import './App.css';
 import firebase from './firebase.js';
 
@@ -6,65 +7,54 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      username: '',
-      items: []
+      votesForOne: 0,
+      votesForTwo: 0,
+      votesForThree: 0
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.handleVote = this.handleVote.bind(this);
   }
 
   componentDidMount() {
-    const itemsRef = firebase.database().ref('items');
-    itemsRef.on('value', (snapshot) => {
-      const items = snapshot.val();
-      const newState = [];
-      for (const item in items) {
-        newState.push({
-          id: item,
-          title: items[item].title,
-          user: items[item].user
-        });
-      }
+    const votesRef = firebase.database().ref('votes');
+    votesRef.on('value', (snapshot) => {
+      const votes = snapshot.val();
       this.setState({
-        items: newState
+        votesForOne: votes.forOne.value,
+        votesForTwo: votes.forTwo.value,
+        votesForThree: votes.forThree.value
       });
     });
   }
 
-  handleChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  }
+  handleVote(e) {
+    const voteFor = e.target.id;
+    const votesRef = firebase.database().ref('votes');
+    let newTotal;
+    if (voteFor === 'forOne') {
+      newTotal = this.state.votesForOne + 1;
+    } else if (voteFor === 'forTwo') {
+      newTotal = this.state.votesForTwo + 1;
+    } else {
+      newTotal = this.state.votesForThree + 1;
+    }
 
-  handleSubmit(e) {
-    e.preventDefault();
-    const itemsRef = firebase.database().ref('items');
-    const item = {
-      user: this.state.username
-    };
-    itemsRef.push(item);
-    this.setState({
-      username: ''
-    });
+    votesRef.child(voteFor).set({ value: newTotal });
   }
 
   render() {
     return (
       <div className="App">
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" name="username" placeholder="type your name" onChange={this.handleChange} value={this.state.username}/>
-          <button>Add your name</button>
-        </form>
-        <ul>
-          {this.state.items.map((item) => {
-            return (
-              <li key={item.id}>
-                <h3>{item.user}</h3>
-              </li>
-            );
-          })}
-        </ul>
+        <div>
+          <h1>Number of votes for 1 {this.state.votesForOne}</h1>
+          <h1>Number of votes for 2 {this.state.votesForTwo}</h1>
+          <h1>Number of votes for 3 {this.state.votesForThree}</h1>
+        </div>
+        <div className="buttons">
+          <Button onClick={this.handleVote} id="forOne" bsStyle="primary">Vote for 1</Button>
+          <Button onClick={this.handleVote} id="forTwo" bsStyle="success">Vote for 2</Button>
+          <Button onClick={this.handleVote} id="forThree" bsStyle="info">Vote for 3</Button>
+        </div>
       </div>     
     );
   }
